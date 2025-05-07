468a6f4fa4bd1fc0a4550a7e75b031fe1bda366e
bconti123 <bryantcconti@gmail.com>
2025-05-06 22:40:23 -0700

    Install Vitest and created config.test.ts

---
+++ server/src/config.test.ts
@@ -0,0 +1,16 @@
+import { describe, test, expect } from "vitest";
+
+describe("config come from env", () => {
+    test("see it works", async () => {
+        process.env.SECRET_KEY = "abc";
+        process.env.PORT = "5001";
+
+        // Re-import
+        const {default:config} = await import("./config");
+        expect(config.SECRET_KEY).toBe("abc");
+        expect(config.PORT).toEqual(5001);
+
+        delete process.env.SECRET_KEY;
+        delete process.env.PORT;
+    })
+}) 
