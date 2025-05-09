0fed30a599049859b08aa2514e67d9a6c509e056
bconti123 <bryantcconti@gmail.com>
2025-05-08 22:14:54 -0700

    Fixed config test

--- server/src/config.test.ts
+++ server/src/config.test.ts
@@ -1,16 +1,45 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, vi } from "vitest";
 
 describe("config come from env", () => {
-    test("see it works", async () => {
+    test("dev env works", async () => {
         process.env.SECRET_KEY = "abc";
         process.env.PORT = "5001";
+        process.env.NODE_ENV = "dev";
+        process.env.DATABASE_URI = "favorite_db";
+
+        vi.resetModules();
 
         // Re-import
-        const { config } = await import("./config");
+        const { config, getDatabaseUri } = await import("./config");
         expect(config.SECRET_KEY).toBe("abc");
         expect(config.PORT).toEqual(5001);
+        expect(config.NODE_ENV).toBe("dev");
+        expect(getDatabaseUri()).toBe("favorite_db");
 
         delete process.env.SECRET_KEY;
         delete process.env.PORT;
+        delete process.env.NODE_ENV;
+        delete process.env.DATABASE_URI;
+        console.debug(process.env.NODE_ENV);
+    });
+    test("test env works", async () => {
+        process.env.SECRET_KEY = "abc";
+        process.env.PORT = "5001";
+        process.env.NODE_ENV = "test";
+
+        vi.resetModules();
+
+        // Re-import
+        const { config, getDatabaseUri } = await import("./config");
+
+        expect(config.SECRET_KEY).toBe("abc");
+        expect(config.PORT).toEqual(5001);
+        expect(config.NODE_ENV).toBe("test");
+        expect(getDatabaseUri()).toBe("database_name_test");
+
+        delete process.env.SECRET_KEY;
+        delete process.env.PORT;
+        delete process.env.NODE_ENV;
+        delete process.env.DATABASE_URI;
     })
 }) 
